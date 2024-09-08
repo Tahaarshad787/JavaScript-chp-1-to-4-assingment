28750e08a738ff809a4d8056fd269853fb70eef5
Tahaarshad787 <tahaarshad311@gmail.com>
2024-09-09 00:43:09 +0500

    add files and folder

---
+++ app.js
@@ -0,0 +1,222 @@
+// chapter 1(Alert)
+// Q1
+
+// alert("JAVAScript Alert");
+
+// Q2                                       
+// alert("Error Please enter a valid password");
+
+// // Q3
+// alert("JAVAScript Alert");
+// alert("Welcome to JS Land... \n Happy Coding");
+
+// // Q4
+// alert("Welcome to JS Land")
+// alert("Happy Coding!  \n Prevent this page from creating additional doalogs.")
+
+// // Q5
+// alert("Hello... I can run JS through my web browser's console");
+
+// // Q6
+// alert("Welcome");
+
+// // Chapter 2
+
+// // Q1
+
+// var username
+
+// // Q2
+// var myname = ("Muhammad Taha");
+// console.log(myname);
+
+// // Q3
+// var message;
+// message = "Hello World";
+// alert(message);
+
+// Q4
+// var studentName = "Taha";
+// var studentAge ="19";
+// var studentEducation = "Certified Mobile  Application Development";
+// alert(studentName);
+// alert(studentAge);
+// alert(studentEducation);
+
+// Q5
+// var word = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert(word);
+
+// Q6
+// var email = "My Email address is example@gmail.com";
+// alert(email);
+
+// // Q7
+// var book = "A smarter way to learn JavaScript"
+// alert("I am trying to learn from the Book " + book);
+
+// Q8
+// var word = "Yes! I can write HTML content through JavaScript";
+// document.write(word);
+
+// Q9
+// var a = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(a);
+
+// Chapter 3
+
+// Q1
+// var age = 15;
+// alert("I am " + age + " year old");
+
+// Q2
+// var number = 14;
+// alert("You have visited this site " + number + " times");
+
+// Q3
+// var birthYear = 2005;
+// document.write("My Birth year is " + birthYear +  "<br>Data type of my ddeclared variable is number  ");
+
+// Q4
+// var visitorName = "John Doe"
+// var productTittle =" XYZ Clothing store"
+// var quantity = "5 T-shirt(s)"
+// document.write(visitorName,productTittle,quantity);
+
+// Chapter 4
+// Q1
+// var age,number,height;
+// console.log();
+
+// Q2
+// 5 legal Variables:
+
+// var $myvar;
+// var _mystring;
+// var name123;
+// var monday458;
+// var input;
+
+// 5 illegal Variables
+
+// var 12;
+// var 2559 num;
+// var 2478 word;
+// var num$;
+// var 12monday;
+
+// Q3 (A)
+// var heading = "<h2> Rule For Naming JS Variables</h2>";
+// document.write(heading);
+
+// Q3 (B)
+// var number = "numbers,";
+// var dollar = " $";
+// var variable = "_";
+
+// document.write("Variable name can only contain " + number,dollar + "and" + variable + ". For example : $my_1stVariable <br> ");
+
+
+// Q3 (C)
+// var word = "letter,";
+// var dollar = " $"; 
+// var variable = "_";
+
+// document.write("Variable must begin with a " + word,dollar + " or" + variable + ". For example : $name, _name or name <br>");
+
+// Q3 (D)
+// var word = "sensitive";
+
+// document.write("Variable names are case" + " " + word + "<br>");
+
+// Q3 (E)
+// var word = "keywords";
+
+// document.write("Variable names should not be JS" + " " + word);
+
+// Chapter 1(ALERT):
+// Q1 (Part 1)
+                                      
+// alert("Error Please enter a Firstname");
+
+// Q1 (Part 2)
+                                      
+// alert("Error Please enter a Lastname");
+
+// Q1 (Part 3)
+                                      
+// alert("Error Please enter a valid Email");
+
+// Q1 (Part 4)
+                                      
+// alert("Error Please enter a valid Phone Number");
+
+// Q1 (Part 5)
+                                      
+// alert("Error Please enter a valid Password");
+
+// Q2
+
+// alert("You're learning JavaScript!");
+
+// Q3
+
+// alert("Welcome to JS Land... \n Happy Coding");
+
+
+// Chapter 2
+
+// Q1
+
+// var FirstName;
+
+// Q2
+
+// var myVariable;
+// myVariable = "Hello world"
+
+// Q3
+
+// var teamName = "Hello";
+// alert(teamName);
+
+// Q4
+
+// var bestMan = "Charlie";
+// bestMan = "Johndoe";
+
+// Chapter 3
+
+// Q1
+
+// var caseQty;
+
+// Q2
+
+// caseQty = 144;
+
+// Q3
+
+// var num = 9;
+
+// Q4
+
+// var result;
+
+// result = 5+7;
+
+// Q5
+
+var merchTotal = 100
+var shippingCharge = 10
+console.log(merchTotal + shippingCharge);
+
+
+
+
+
+
+
+
+
+
